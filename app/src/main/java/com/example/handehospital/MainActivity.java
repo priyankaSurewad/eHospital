6978495e97327e73f2948db18d98d864e4027f93
Girish <girishy@geo.cdacitpark.in>
2019-04-10 18:06:06 +0530

    Initial App

---
+++ app/src/main/java/com/example/handehospital/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.handehospital;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    GridView gridView;
+    ArrayList<Item> gridArray = new ArrayList<Item>();
+    CustomGridViewAdapter customGridAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //set grid view item
+        Bitmap appontments = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_appoint);
+        Bitmap contactIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_contact);
+        Bitmap our_offers = BitmapFactory.decodeResource(this.getResources(), R.drawable.our_offers);
+        Bitmap req_amb = BitmapFactory.decodeResource(this.getResources(), R.drawable.req_ambulance);
+        Bitmap our_spl = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_home);
+        Bitmap our_pkg = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_package);
+
+        gridArray.add(new Item(our_spl,"Specialities"));
+        gridArray.add(new Item(contactIcon,"Contact Us"));
+        gridArray.add(new Item(appontments,"Appointments"));
+        gridArray.add(new Item(our_offers,"On Going Offers"));
+        gridArray.add(new Item(req_amb,"Request Ambulance"));
+        gridArray.add(new Item(our_pkg,"Health Packages"));
+        gridView = (GridView) findViewById(R.id.gridView1);
+        customGridAdapter = new CustomGridViewAdapter(this, R.layout.row_grid, gridArray);
+        gridView.setAdapter(customGridAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+    /*    if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } /*else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
